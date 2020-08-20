5f040a7a4f664d36a7a38c294b867212a2e9fba5
Eva Maina <evajohnson714@gmail.com>
2020-08-20 13:20:01 +0300

    [Chore #174410762]Passport Setup

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
+passport.use(new GoogleStrategy())
 
-app.get('/', (req, res) => {
-    res.send({ Hi : 'there' });
-});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
