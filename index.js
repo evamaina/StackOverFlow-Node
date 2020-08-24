92747d249358a6f8aa6945efe873f63fb595720b
Eva Maina <evajohnson714@gmail.com>
2020-08-24 09:56:59 +0300

    [Feature #174410762] google oauth authentication  (#3)
    * [Chore #174410762]Passport Setup
    
    * [ #174410762]Authentication with google oauth

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const express = require('express');
+const express = require("express");
+require("./services/passport");
+
 const app = express();
+require("./routes/authRoutes")(app);
 
-
-app.get('/', (req, res) => {
-    res.send({ Hi : 'there' });
-});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
