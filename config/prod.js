5613f127335996d8044e51adfcc62c90b335a0fd
Eva Maina <evajohnson714@gmail.com>
2020-08-27 07:20:32 +0300

    [Chore #174524612] Set Up Environment variables  (#6)
    * [Chore #174524612]Set up environment variables
    
    * [Chore #174524612] Fix heroku proxy issues

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+    googleRedirectURI: "https://immense-ravine-35702.herokuapp.com",
+};
