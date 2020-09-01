bc7fd47ba95bfb8119f04ac752c1ad0cf5a42745
Eva Maina <evajohnson714@gmail.com>
2020-09-02 01:52:34 +0300

    [Feature #174590679] configuring routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./Header";
+const DashBoard = () => <h2>DashBoard</h2>;
+const PostQuestion = () => <h2>Post Question</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-    return <div>Hi There!</div>;
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/questions" component={DashBoard} />
+                    <Route path="/questions/post" component={PostQuestion} />
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
