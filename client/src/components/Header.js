70378c6d37a6dd3640ee8fb8e4e2a413eef50f2a
Eva Maina <evajohnson714@gmail.com>
2020-09-02 03:25:34 +0300

    [Feature #174590679] designing the header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 
 class Header extends Component {
     render() {
-        return <div>Header</div>;
+        return (
+            <nav>
+                <div className="navWrapper">
+                    <a className="left brand-logo">StackOverFlow</a>
+                    <ul className="right">
+                        <li>
+                            <a>Login With Google</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
     }
 }
 export default Header;
